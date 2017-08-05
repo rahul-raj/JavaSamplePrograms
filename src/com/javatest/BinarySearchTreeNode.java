c0fd520b0ec26e084dc6f9ac758fc6237d9cb6de
Rahul Raj <omrahulrajcse@gmail.com>
2017-08-05 14:20:26 +0530

    added code to find mirror image of a node in binary tree. added code to find path of a node in binary search tree.

--- src/com/javatest/BinarySearchTreeNode.java
+++ src/com/javatest/BinarySearchTreeNode.java
@@ -7,6 +7,7 @@ public class BinarySearchTreeNode {
     int data;
     BinarySearchTreeNode left;
     BinarySearchTreeNode right;
+    static StringBuilder sc = new StringBuilder("");
     BinarySearchTreeNode(){}
     BinarySearchTreeNode(int data){
         this.data=data;
@@ -30,15 +31,57 @@ public class BinarySearchTreeNode {
 
     }
 
+    static StringBuilder searchNode(BinarySearchTreeNode binarySearchTreeNode,int data){
+
+        if(binarySearchTreeNode!=null){
+           if(binarySearchTreeNode.data>data){
+             sc=sc.append("L");
+             sc=searchNode(binarySearchTreeNode.left,data);
+           }
+           else if(binarySearchTreeNode.data<data){
+               sc=sc.append("R");
+               sc=searchNode(binarySearchTreeNode.right,data);
+           }
+           StringBuilder temp=new StringBuilder(sc);
+           sc=new StringBuilder("");
+           return temp;
+        }
+        else
+            return new StringBuilder("");
+    }
+
+    static int findMirrorNode(BinarySearchTreeNode binarySearchTreeNode, String path){
+        int mirrorImage=-1;
+        if(binarySearchTreeNode!=null && path == null){
+            return binarySearchTreeNode.data;
+        }
+        else if(binarySearchTreeNode!=null && path.length()==1){
+            if(path.charAt(0)=='L'){
+                mirrorImage = findMirrorNode(binarySearchTreeNode.right, null);
+            }
+            else if(path.charAt(0)=='R'){
+                mirrorImage = findMirrorNode(binarySearchTreeNode.left, null);
+            }
+        }
+        else if(binarySearchTreeNode!=null && path.length()>1){
+            if(path.charAt(0)=='L'){
+                mirrorImage= findMirrorNode(binarySearchTreeNode.right, path.substring(1,path.length()));
+            }
+            else if(path.charAt(0)=='R'){
+                mirrorImage= findMirrorNode(binarySearchTreeNode.left, path.substring(1,path.length()));
+            }
+        }
+
+        return mirrorImage;
+    }
+
     public static void main(String[] args) {
       BinarySearchTreeNode node = new BinarySearchTreeNode(12);
       node=insertNode(node,10);
         node=insertNode(node,11);
         node=insertNode(node,9);
-        System.out.println(node.data);
-      System.out.println(node.left.data);
-        System.out.println(node.left.right.data);
-        System.out.println(node.left.left.data);
+        node=insertNode(node,13);
+        System.out.println(findMirrorNode(node,searchNode(node,11).toString()));
 
     }
 }
